48c4d530cc1a7edef1d46ee0c12557e5f3662fc6
ryo <hideyoshi.adm@gmail.com>
2025-03-04 03:17:52 +0900

    一旦コミット

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // "/" にアクセスされた場合のハンドラー
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, ECS on port 8080!(updated2)")
+	fmt.Fprintln(w, "Hello, ECS on port 8080!(updated3)")
 }
 
 // "/health" にアクセスされた場合のヘルスチェックハンドラー
