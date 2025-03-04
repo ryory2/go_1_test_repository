bca76b59ff0810447f4b599e04f2ff92dd023c1f
ryo <hideyoshi.adm@gmail.com>
2025-03-05 03:24:11 +0900

    GI用定義の更新

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // "/" にアクセスされた場合のハンドラー
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, ECS on port 8080!(updated6)")
+	fmt.Fprintln(w, "Hello, ECS on port 8080!(updated8)")
 }
 
 // "/health" にアクセスされた場合のヘルスチェックハンドラー
